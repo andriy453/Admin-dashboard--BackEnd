2984c01bc442845926161aaa588ab9fbd991447a
andriy <evicandrij@gmail.com>
2024-03-29 20:33:48 +0200

    fix controlers

--- controllers/order-controller.js
+++ controllers/order-controller.js
@@ -1,35 +1,38 @@
-const { ctrlWrapper,HttpError } = require("../helpers");
-const { Order } = require('../models/orders')
+const { ctrlWrapper, HttpError } = require("../helpers");
+const { Order } = require("../models/orders");
 
- const ListOrder = async (req, res, next) => {
-    try {
-        const { page = 1, name,...quer  } = req.query;
-        const pageSize = 5;
-        const skip = (page - 1) * pageSize;
-        const totalOrder = await Order.countDocuments(quer);
-    
-        if (totalOrder === 0) {
-          throw new HttpError(404, `Order not found`);
-        }
-        if (skip >= totalOrder) {
-          throw new HttpError(404, `No more Order available on page ${page}`);
-        }
-        const order = await Order.find(quer).skip(skip).limit(pageSize);
-    
-        res.status(200).json({
-          totalOrder,
-          pageSize,
-          currentPage: page,
-          totalPages: Math.ceil(totalOrder / pageSize),
-          order,
-        });
-      } catch (error) {
-        console.error("Error fetching Order:", error);
-        next(error);
-      }
+const ListOrder = async (req, res, next) => {
+  try {
+    const { page = 1, name, ...quer } = req.query;
+    const pageSize = 5;
+    const skip = (page - 1) * pageSize;
+    let query = {};
+    if (name) {
+      query.name = { $regex: new RegExp(name, "i") };
+    }
+    const totalOrder = await Order.countDocuments(query);
+
+    if (totalOrder === 0) {
+      throw new HttpError(404, `Order not found`);
+    }
+    if (skip >= totalOrder) {
+      throw new HttpError(404, `No more Order available on page ${page}`);
+    }
+    const order = await Order.find(quer).skip(skip).limit(pageSize);
+
+    res.status(200).json({
+      totalOrder,
+      pageSize,
+      currentPage: page,
+      totalPages: Math.ceil(totalOrder / pageSize),
+      order,
+    });
+  } catch (error) {
+    console.error("Error fetching Order:", error);
+    next(error);
+  }
 };
 
-
 module.exports = {
-    ListOrder: ctrlWrapper(ListOrder),
+  ListOrder: ctrlWrapper(ListOrder),
 };
--- controllers/supplier-controller.js
+++ controllers/supplier-controller.js
@@ -1,69 +1,72 @@
-const { ctrlWrapper,HttpError } = require("../helpers");
-const {Supplier} = require('../models/supplier')
+const { ctrlWrapper, HttpError } = require("../helpers");
+const { Supplier } = require("../models/supplier");
 const ListSupplier = async (req, res, next) => {
-    try {
-        const { page = 1, name,...quer  } = req.query;
-        const pageSize = 5;
-        const skip = (page - 1) * pageSize;
-        const totalSupplier = await Supplier.countDocuments(quer);
-    
-        if (totalSupplier === 0) {
-          throw new HttpError(404, `Supplier not found`);
-        }
-        if (skip >= totalSupplier) {
-          throw new HttpError(404, `No more supplier available on page ${page}`);
-        }
-        const supplier = await Supplier.find(quer).skip(skip).limit(pageSize);
-    
-        res.status(200).json({
-          totalSupplier,
-          pageSize,
-          currentPage: page,
-          totalPages: Math.ceil(totalSupplier / pageSize),
-          supplier,
-        });
-      } catch (error) {
-        console.error("Error fetching supplier:", error);
-        next(error);
-      }
+  try {
+    const { page = 1, name, ...quer } = req.query;
+    const pageSize = 5;
+    const skip = (page - 1) * pageSize;
+    let query = {};
+    if (name) {
+      query.name = { $regex: new RegExp(name, "i") };
+    }
+    const totalSupplier = await Supplier.countDocuments(query);
+
+    if (totalSupplier === 0) {
+      throw new HttpError(404, `Supplier not found`);
+    }
+    if (skip >= totalSupplier) {
+      throw new HttpError(404, `No more supplier available on page ${page}`);
+    }
+    const supplier = await Supplier.find(quer).skip(skip).limit(pageSize);
+
+    res.status(200).json({
+      totalSupplier,
+      pageSize,
+      currentPage: page,
+      totalPages: Math.ceil(totalSupplier / pageSize),
+      supplier,
+    });
+  } catch (error) {
+    console.error("Error fetching supplier:", error);
+    next(error);
+  }
 };
 
 const addSupplier = async (req, res, next) => {
-    try {
-      const result = await Supplier.create({ ...req.body });
-      res.status(201).json({ data: result });
-    } catch (error) {
-      next(error);
-    }
-  };
+  try {
+    const result = await Supplier.create({ ...req.body });
+    res.status(201).json({ data: result });
+  } catch (error) {
+    next(error);
+  }
+};
 
-  const updateSupplier = async (req, res, next) => {
-    const { supplierId } = req.params;
-    const result = await Supplier.findByIdAndUpdate(supplierId, req.body, {
-      new: true,
-    });
-    if (!result) {
-      HttpError(404,  `Supplier with id=${supplierId} not found`);
-    }
-    res.status(200).json(result);
-  };
+const updateSupplier = async (req, res, next) => {
+  const { supplierId } = req.params;
+  const result = await Supplier.findByIdAndUpdate(supplierId, req.body, {
+    new: true,
+  });
+  if (!result) {
+    HttpError(404, `Supplier with id=${supplierId} not found`);
+  }
+  res.status(200).json(result);
+};
 
-  const deleteById = async (req, res, next) => {
-    const { supplierId } = req.params;
-    const result = await Supplier.findByIdAndDelete(supplierId);
-  
-    if (!result) {
-      throw HttpError(404, `Supplier with id=${supplierId} not found`);
-    }
-    res.json({
-      message: "Supplier success",
-    });
-  };
+const deleteById = async (req, res, next) => {
+  const { supplierId } = req.params;
+  const result = await Supplier.findByIdAndDelete(supplierId);
 
+  if (!result) {
+    throw HttpError(404, `Supplier with id=${supplierId} not found`);
+  }
+  res.json({
+    message: "Supplier success",
+  });
+};
 
 module.exports = {
-    ListSupplier: ctrlWrapper(ListSupplier),
-    addSupplier:ctrlWrapper(addSupplier),
-    updateSupplier:ctrlWrapper(updateSupplier),
-    deleteById:ctrlWrapper(deleteById),
+  ListSupplier: ctrlWrapper(ListSupplier),
+  addSupplier: ctrlWrapper(addSupplier),
+  updateSupplier: ctrlWrapper(updateSupplier),
+  deleteById: ctrlWrapper(deleteById),
 };
